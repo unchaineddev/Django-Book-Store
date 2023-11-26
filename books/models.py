66708060cbf27294ee09c9717df8bedef4a8b6cd
yusuf <yusuftechie+git@gmail.com>
2023-11-26 13:15:56 +0530

    #2 migration

--- books/models.py
+++ books/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
 # Create your models here.
 
@@ -6,5 +7,10 @@ from django.db import models
 class Book(models.Model):
     title = models.CharField(max_length=50)  # character field 
     author = models.CharField(max_length=50)
-    rating = models.IntegerField()
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    is_best_selling = models.BooleanField(default=False)
     # id = models.AutoField() # added automatically (unique identifier)
+
+
+    def __str__(self):
+        return f'{self.title}--{self.author} ({self.rating})'
