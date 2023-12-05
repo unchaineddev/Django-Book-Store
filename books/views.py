580425a7abb5d24ec23bbd93431c6bad670da10b
yusuf <yusuftechie+git@gmail.com>
2023-12-05 21:23:02 +0530

    aggregation & fixing

--- books/views.py
+++ books/views.py
@@ -1,14 +1,19 @@
 from django.http import Http404
 from django.shortcuts import get_object_or_404, render
-
+from django.db.models import Avg # For Aggregation
 # Create your views here.
 
 from .models import Book
 
 def index(request):
-    books = Book.objects.all()
+    books = Book.objects.all().order_by("-title")   # orders by data; minus is desc
+    num_books = books.count()
+    avg_rate = books.aggregate(Avg("rating")) 
+    
     return render(request, "books/index.html", {
-        "books": books
+        "books": books,
+        "total_number_of_books": num_books,
+        "average_rate": avg_rate
     })
 
 
