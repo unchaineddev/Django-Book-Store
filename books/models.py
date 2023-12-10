54b900456732af55dc101b80ce33587ea2cef182
yusuf <yusuftechie+git@gmail.com>
2023-12-10 11:47:34 +0530

    Adding relations between Author and Book

--- books/models.py
+++ books/models.py
@@ -1,3 +1,4 @@
+from operator import mod
 from django.db import models
 from django.core.validators import MinValueValidator, MaxValueValidator
 from django.db.models.fields.files import default_storage
@@ -8,9 +9,15 @@ from django.utils.text import slugify   # modify slug
 # Create your models here.
 
 
+class Author(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+
+
 class Book(models.Model):
     title = models.CharField(max_length=50)  # character field 
-    author = models.CharField(max_length=50)
+    # author = models.CharField(max_length=50)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE, null=True) # or PROTECT
     rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
     is_best_selling = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True, null=False, db_index=True) # Harry Potter 1 => harry-potter-1
